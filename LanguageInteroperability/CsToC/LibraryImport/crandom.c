0017e75b90891bc7e8fdfd6ebc256fed3ff489d4
michelenatale <104991886+michelenatale@users.noreply.github.com>
2026-01-26 12:39:42 +0100

    Delete LanguageInteroperability directory

--- LanguageInteroperability/CsToC/LibraryImport/crandom.c
+++
@@ -1,9 +0,0 @@
-﻿#include <stdlib.h> 
-#include <time.h> 
-
-__declspec(dllexport) void fill_random_lib_import(unsigned char* buffer, int length)
-{ 
-  srand((unsigned)time(NULL)); 
-  for (int i = 0; i < length; i++) 
-    buffer[i] = rand() % 256; 
-}
--- LanguageInteroperability/CsToC/PInvoke/cadd.c
+++
@@ -1,6 +0,0 @@
-﻿
-
-__declspec(dllexport) int add(int a, int b)
-{
-  return a + b; 
-}
